69371b445994d8608244c7b156cf6eaba3bd9c6d
eliird <irdali1996@gmail.com>
2023-12-25 11:56:56 +0900

    Finished watching dijkstra

---
+++ graphs.go
@@ -0,0 +1,18 @@
+package main
+
+//Cyclic Graph Can visit three nodes and come back
+//acyclic no cycle in the graphs
+//Directed and Undirected
+
+//DAG directed and acyclic
+
+//node is vertex of the graph
+//edge is the connection between the nodes
+
+//Represent in two ways
+//Adj List
+//Adj matrix  Takes a lot of memory and setting up so is not common in practice O(V**2) especially in sparsely connected
+
+//Adj List Would be same as linked list
+//Each node will contain all the items that can be traversed from that node as tuple
+//You know how to represent adjacency matrix
